4728f0a8107491ac9c467e3bc6e7a957755c4c56
DenisShipilov <Shipilov2D90@gmail.com>
2014-10-28 02:27:15 +0300

    Added server side validation

--- src/main/java/actions/order/form/OrderForm.java
+++ src/main/java/actions/order/form/OrderForm.java
@@ -1,28 +1,83 @@
 package actions.order.form;
 
-import org.apache.struts.action.*;
 
-import javax.servlet.http.HttpServletRequest;
+import org.apache.struts.validator.ValidatorForm;
+
 import java.math.BigInteger;
+import java.util.Date;
 
 /**
  * Created by Denis_Shipilov on 13.10.2014.
  */
-public class OrderForm extends ActionForm {
+public class OrderForm extends ValidatorForm {
 
-    BigInteger bil ;
+    private Date registrationdate;
 
-    @Override
-    public ActionErrors validate(ActionMapping mapping, HttpServletRequest request) {
-        ActionErrors errors = new ActionErrors();
-        if(bil == null || String.valueOf(bil).length() > 20) {
-            errors.add("bil",new ActionMessage("bil.wrong.length"));
-        }
-        return errors;
+    private Date expiredate;
+
+    private  BigInteger bill ;
+
+    private BigInteger ordernumber;
+
+    private String username;
+
+    private Integer userid;
+
+    private String useremail;
+
+    public Date getRegistrationdate() {
+        return registrationdate;
     }
 
-    @Override
-    protected ActionServlet getServlet() {
-        return super.getServlet();
+    public void setRegistrationdate(Date registrationdate) {
+        this.registrationdate = registrationdate;
+    }
+
+    public Date getExpiredate() {
+        return expiredate;
+    }
+
+    public void setExpiredate(Date expiredate) {
+        this.expiredate = expiredate;
+    }
+
+    public BigInteger getBill() {
+        return bill;
+    }
+
+    public void setBill(BigInteger bill) {
+        this.bill = bill;
+    }
+
+    public BigInteger getOrdernumber() {
+        return ordernumber;
+    }
+
+    public void setOrdernumber(BigInteger ordernumber) {
+        this.ordernumber = ordernumber;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getUserid() {
+        return userid;
+    }
+
+    public void setUserid(Integer userid) {
+        this.userid = userid;
+    }
+
+    public String getUseremail() {
+        return useremail;
+    }
+
+    public void setUseremail(String useremail) {
+        this.useremail = useremail;
     }
 }
---
+++ src/main/webapp/order/pages/EditOrder.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Dенис
+  Date: 28.10.2014
+  Time: 2:01
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Edit Order</title>
+</head>
+<body>
+
+
+</body>
+</html>
